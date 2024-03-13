0e3be076fe7f3a6841e33aeae688efb2b2735fbc
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-13 11:34:40 +0200

    add ay

--- src/view/Index.tsx
+++ src/view/Index.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Dimensions, Image, SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import {Dimensions, Image, SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 const screenHeight = Dimensions.get('screen').height;
+// const screenWidth = Dimensions.get('screen').width;
 const Index = () => {
   return (
     <SafeAreaView style={styles.section}>
-        <StatusBar backgroundColor={'#798DD1'}/>
-        <Image width={45} source={require("../assets/visitor/download.png")}/>
-      <Text style={styles.section1}>Michael jackson</Text>
-      <Text style={styles.section2}>Michael jackson</Text>
+        <StatusBar backgroundColor={'#87CEEB'}/>
+        <Image width={50} height={78} style={styles.image} source={require('../assets/visitor/download.png')}/>
+      <Text style={styles.section1}>Adventist Youth</Text>
+      <Text style={styles.section2}>Tape here to continue</Text>
+      <Text>V</Text>
     </SafeAreaView>
   );
 };
@@ -18,17 +20,28 @@ const styles = StyleSheet.create({
     height:screenHeight,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor:'#798DD1',
+    backgroundColor:'#87CEEB',
   },
   section1: {
     color: 'white',
     fontSize: 30,
     fontWeight: 'bold',
+    textShadowColor:'black',
+    textShadowRadius:10,
   },
   section2: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 15,
     marginTop:screenHeight / 2,
+  },
+  section3:{
+    backgroundColor:'white',
+    padding:4,
+    borderRadius:10,
+  },
+  image:{
+height:100,
+width:100,
   },
 });
 export default Index;
