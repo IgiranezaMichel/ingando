f3e9a8d37f95ac88f0df283b7b34f2fccec8398f
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-14 11:37:05 +0200

    remove const to let

--- src/hooks/screen.tsx
+++ src/hooks/screen.tsx
@@ -1,7 +1,7 @@
 import {Dimensions} from 'react-native';
 
 export const Screen = () => {
-  const width = Dimensions.get('screen').height;
-  const height = Dimensions.get('screen').width;
+  let width = Dimensions.get('screen').height;
+  let height = Dimensions.get('screen').width;
   return {width, height};
 };
