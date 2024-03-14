7d93e40cf24dece90137a47bb66a19c992234221
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-14 15:25:19 +0200

    reversing

--- src/hooks/screen.tsx
+++ src/hooks/screen.tsx
@@ -1,7 +1,7 @@
 import {Dimensions} from 'react-native';
 
 export const Screen = () => {
-  let width = Dimensions.get('screen').height;
-  let height = Dimensions.get('screen').width;
+  let width = Dimensions.get('screen').width;
+  let height = Dimensions.get('screen').height;
   return {width, height};
 };
