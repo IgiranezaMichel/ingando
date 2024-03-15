63ff6d00718ecf52fed560340cf57478cc42c3a2
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-15 15:03:24 +0200

    add lottie file

--- src/view/userPages/index.tsx
+++ src/view/userPages/index.tsx
@@ -1,127 +1,41 @@
-/* eslint-disable react/react-in-jsx-scope */
-import {
-  Image,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
+import LottieView from 'lottie-react-native';
 import {Screen} from '../../hooks/screen';
 
 export const Home = () => {
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor={'blue'} />
-      <View style={styles.topCOntainer}>
+      <View style={styles.subLogoContainer}>
         <Image style={styles.logo} source={require('../../assets/logo.png')} />
-        <View style={styles.topContainerItem}>
-          <TextInput
-            style={styles.searchInput}
-            placeholder="Search"
-            placeholderTextColor={'grey'}
-          />
-          <TouchableOpacity style={styles.searchBtn}>
-            <Image
-              style={styles.searchImg}
-              source={require('../../assets/visitor/icons/searching.png')}
-            />
-          </TouchableOpacity>
-        </View>
+        {/* Enclose the text within a Text component */}
       </View>
-      <View style={styles.topSubContainer}>
-        <Text style={styles.ingandoText}>Available Ingando</Text>
-        <View style={styles.topSubContainerOption}>
-          <TouchableOpacity style={styles.activeBtn}>
-            <Text style={styles.ingandoOptionText}>All</Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text style={styles.ingandoOptionText}>History</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <ScrollView>
-        <Text style={styles.ingandoText}>Available Ingando</Text>
-      </ScrollView>
+      <LottieView autoPlay={true}
+        source={require('../../assets/Lottie.json')}
+        style={{width: 100, height: 100}}
+      />
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
     height: Screen().height,
+    width: Screen().width,
   },
-  topCOntainer: {
-    display: 'flex',
-    height: Screen().height / 6,
+  subLogoContainer: {
     backgroundColor: 'blue',
-    justifyContent: 'center',
+    height: Screen().width * 0.2,
+    display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   logo: {
-    width: 50,
-    height: 50,
-    alignSelf: 'center',
-    borderWidth: 2,
+    width: Screen().width / 8,
+    height: Screen().width / 8,
     borderColor: 'black',
-    borderRadius: 10,
-    marginBottom: Screen().height / 120,
-  },
-  topContainerItem: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: Screen().width * 0.9,
-    marginBottom: 3,
-    borderColor: 'white',
     borderWidth: 2,
-  },
-  searchBtn: {
-    width: 13,
-  },
-  searchImg: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'white',
-    padding: 12,
-  },
-  searchInput: {
-    width: Screen().width / 1.33,
-  },
-  ingandoText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontFamily: 'fantasy',
-    margin: 10,
-    fontSize: Screen().width * 0.05,
-  },
-  ingandoOptionText: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontFamily: 'fantasy',
-    margin: 10,
-    fontSize: Screen().width * 0.05,
-  },
-  topSubContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: Screen().width,
-    backgroundColor: 'blue',
-    marginTop: 2,
-    padding: 3,
-  },
-  topSubContainerOption: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignContent:'flex-end',
-    borderWidth: 2,
-    borderColor: 'white',
-    backgroundColor: 'white',
-  },
-  activeBtn: {
-    backgroundColor: 'rgb(29, 110, 181)',
-    display: 'flex',
-    alignSelf: 'flex-end',
   },
 });
