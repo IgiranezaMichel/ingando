da5f6682d5003eeecd697e5303561ce499d1aa44
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-26 17:11:30 +0200

    grid

---
+++ src/style/Grid.tsx
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+export const Grid = StyleSheet.create({
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  spaceBtn: {
+    justifyContent: 'space-between',
+  },
+  colSm1: {width: ' 8.33333%'},
+  colSm2: {width: '16.66667%'},
+  colSm3: {width: '25%'},
+  colSm4: {width: '33.33333%'},
+  colSm5: {width: '41.66667%'},
+  colSmm6: {width: '50%'},
+  colSmm7: {width: '58.33333%'},
+  colSmm8: {width: '66.66667%'},
+  colSmm9: {width: '75%'},
+  colSmm10: {width: '83.33333%'},
+  colSmm11: {width: '91.66667%'},
+  colSmm12: {width: '100%'},
+});
