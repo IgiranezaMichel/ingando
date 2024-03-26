e1b99d49d69b3f1e1939458a1b2b827234c823a1
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-26 19:38:18 +0200

    add padding

---
+++ src/style/Padding.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const Padding = StyleSheet.create({
+  p0: {padding: 0},
+  p1: {padding: '1%'},
+  p2: {padding: '2%'},
+  p3: {padding: '3%'},
+  p4: {padding: '4%'},
+  p5: {padding: '5%'},
+  p6: {padding: '6%'},
+});
