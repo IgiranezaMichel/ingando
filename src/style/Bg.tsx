edc614d1edb1bef707c5c2cb6ddbafdcfbf7e81e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-26 17:11:08 +0200

    Add bg

---
+++ src/style/Bg.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const Bg = StyleSheet.create({
+  bgPrimary: {
+    backgroundColor: '#007bff',
+  },
+  bgSuccess: {
+    backgroundColor: '#28a745',
+  },
+  bgDanger: {
+    backgroundColor: '#dc3545',
+  },
+  bgMuted: {
+    backgroundColor: '#6c757d',
+  },
+});
