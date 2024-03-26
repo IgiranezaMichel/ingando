d39195f452e5a1b073534c4a7a56b74641ddbfef
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-26 17:18:25 +0200

    add border

---
+++ src/style/Border.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const Border = StyleSheet.create({
+  border: {borderWidth: 1},
+  border0: {borderWidth: 0},
+  border1: {borderWidth: 2},
+  border2: {borderWidth: 3},
+  border3: {borderWidth: 4},
+  border4: {borderWidth: 5},
+  border5: {borderWidth: 6},
+  borderPrimary: {borderColor: '#007bff'},
+  borderDark: {borderColor: 'blak'},
+  borderSuCess: {borderColor: '#28a745'},
+  borderDanger: {borderColor: '#db3545'},
+  borderMuted: {borderColor: '#6c757d'},
+});
