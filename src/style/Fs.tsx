239d40082c4858598a1f3afa564b1fdb9370c3c5
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-26 20:24:29 +0200

    add display

---
+++ src/style/Fs.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const FontSize = StyleSheet.create({
+  display1: {
+    fontSize: 48,
+  },
+  display2: {
+    fontSize: 36,
+  },
+  display3: {
+    fontSize: 24,
+  },
+  display4: {
+    fontSize: 12,
+  },
+});
