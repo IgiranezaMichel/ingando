92e77c1718ef780a2bbf841b76f433022bbf31e7
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-28 13:01:32 +0200

    add camp name

--- src/modal/campDetail.tsx
+++ src/modal/campDetail.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/react-in-jsx-scope */
-import {Modal, StatusBar, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Modal,
+  ScrollView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {Bg} from '../style/Bg';
 import {Txt} from '../style/Text';
 import {Card} from '../style/Card';
@@ -11,76 +18,104 @@ import {FontSize} from '../style/Fs';
 import {Border} from '../style/Border';
 import LottieView from 'lottie-react-native';
 import {Screen} from '../hooks/screen';
+import {TimeLine} from './timeLine';
+import {useModalContext} from '../context/modalContext';
 
 export const CampDetailModal = () => {
+  const {isModalVisible, setIsModalVisible} = useModalContext();
   return (
-    <Modal visible={true} style={[Bg.bgPrimary]}>
+    <Modal
+      animationType="slide"
+      visible={isModalVisible}
+      style={[Bg.bgPrimary]}>
       <StatusBar hidden />
-      <View
-        style={[
-          Bg.bgPrimary,
-          {display: 'flex', justifyContent: 'center', height: Screen.height},
-        ]}>
+      <ScrollView
+        style={[{display: 'flex', height: Screen.height}, Bg.bgPrimary]}>
         <View
           style={[
-            Card.card,
-            Padding.p0,
-            rounded.rounded0,
-            Grid.mAuto,
-            Grid.colSm11,
+            {
+              display: 'flex',
+              justifyContent: 'center',
+              height: Screen.height,
+              marginBottom: '25%',
+            },
           ]}>
-          <View style={[Border.borderBottom, Padding.p2]}>
-            <View style={[Grid.row, Grid.spaceBtn]}>
-              <View>
+          <View
+            style={[
+              Card.card,
+              Padding.p0,
+              rounded.rounded0,
+              Grid.mAuto,
+              Grid.colSm11,
+            ]}>
+            <View style={[Border.borderBottom, Padding.p2]}>
+              <View style={[Grid.row, Grid.spaceBtn]}>
                 <View>
-                  <Text style={[Txt.fwBolder, Txt.textPrimary]}>Camp name</Text>
+                  <View>
+                    <Text style={[Txt.fwBolder, Txt.textPrimary]}>
+                      Camp name
+                    </Text>
+                  </View>
+                  <View style={[Grid.row]}>
+                    <LottieView
+                      autoPlay={true}
+                      source={require('../assets/lotties/location.json')}
+                      style={{width: 30, height: 30}}
+                    />
+                    <Text style={[Txt.fwBolder, Txt.textDark]}>Location</Text>
+                  </View>
                 </View>
-                <View style={[Grid.row]}>
-                  <LottieView
-                    autoPlay={true}
-                    source={require('../assets/lotties/location.json')}
-                    style={{width: 30, height: 30}}
-                  />
-                  <Text style={[Txt.fwBolder, Txt.textDark]}>Location</Text>
+                <View>
+                  <Text style={[Txt.fwBolder, Txt.textPrimary]}>Deadline</Text>
                 </View>
-              </View>
-              <View>
-                <Text style={[Txt.fwBolder, Txt.textPrimary]}>Deadline</Text>
               </View>
             </View>
-          </View>
-          <Text style={[Txt.textDark, Padding.p1]}>
-            Description and setting the height dynamically based on the device's
-            window height, we achieve similar behavior to using
-          </Text>
-        </View>
-        <View style={[Grid.row, Grid.colSm11, Grid.mAuto]}>
-          <View style={[Card.card, Grid.colSm6, Grid.m0, rounded.rounded0]}>
-            <Text style={[Txt.textDark]}>Total Cost</Text>
-            <Text style={[FontSize.display1, Txt.textDark, Txt.fwBolder]}>
-              12 frw
+            <Text style={[Txt.textDark, Padding.p1]}>
+              Description and setting the height dynamically based on the
+              device's window height, we achieve similar behavior to using
             </Text>
-          </View>
-          <View style={[Grid.colSm6]}>
-            <View style={[Card.card, Grid.colSm12, Grid.m0, rounded.rounded0]}>
-              <Text style={[Txt.textDark]}>Last</Text>
-              <Text style={[FontSize.display3, Txt.textDark, Txt.fwBolder]}>
-                12 weeks
+            <TimeLine
+              headerTxt="Week of prayer"
+              headerStyle={[Txt.fwBolder, Txt.textDark]}>
+              <Text style={[Txt.textPrimary]}>
+                Description and setting the height dynamically based on the
+                device's window height, we achieve similar behavior to using
               </Text>
-              <Text style={[Txt.textDark, FontSize.display5]}>
-                monday-12-2024 -thursday-12-2024
+            </TimeLine>
+          </View>
+          <View style={[Grid.row, Grid.colSm11, Grid.mAuto]}>
+            <View style={[Card.card, Grid.colSm6, Grid.m0, rounded.rounded0]}>
+              <Text style={[Txt.textDark]}>Total Cost</Text>
+              <Text style={[FontSize.display1, Txt.textDark, Txt.fwBolder]}>
+                12 frw
               </Text>
             </View>
-            <View style={[Card.card, Grid.colSm12, Grid.m0, rounded.rounded0]}>
-              <Text style={[Txt.textDark]}>Total Applicant</Text>
-              <Text style={[FontSize.display1, Txt.textDark, Txt.fwBolder]}>
-                12
-              </Text>
+            <View style={[Grid.colSm6]}>
+              <View
+                style={[Card.card, Grid.colSm12, Grid.m0, rounded.rounded0]}>
+                <Text style={[Txt.textDark]}>Last</Text>
+                <Text style={[FontSize.display3, Txt.textDark, Txt.fwBolder]}>
+                  12 weeks
+                </Text>
+                <Text style={[Txt.textDark, FontSize.display5]}>
+                  monday-12-2024 -thursday-12-2024
+                </Text>
+              </View>
+              <View
+                style={[Card.card, Grid.colSm12, Grid.m0, rounded.rounded0]}>
+                <Text style={[Txt.textDark]}>Total Applicant</Text>
+                <Text style={[FontSize.display1, Txt.textDark, Txt.fwBolder]}>
+                  12
+                </Text>
+              </View>
             </View>
           </View>
         </View>
-      </View>
+      </ScrollView>
       <TouchableOpacity
+        onPress={() => {
+          setIsModalVisible(false);
+        }}
         style={[
           {
             width: 70,
@@ -94,6 +129,10 @@ export const CampDetailModal = () => {
             alignItems: 'center',
             position: 'absolute',
             zIndex: 1,
+            borderLeftColor: 'black',
+            borderLeftWidth: 5,
+            borderBottomColor: 'black',
+            borderBottomWidth: 5,
           },
         ]}>
         <Text
